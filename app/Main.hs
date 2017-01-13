c1c81e4be9128010d2277b54a8cb6a043c5ea5e0
sgf <sgf.dma@gmail.com>
2017-01-13 17:37:40 +0300

    chg: main. Print original and decoded strings separated by `|`.

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE CPP #-}
 
-import qualified Data.ByteString.Char8 as C
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import qualified Data.Text.Encoding.UTF7.IMAP as T
+import qualified Data.ByteString.Char8          as C
+import qualified Data.Text                      as T
+import qualified Data.Text.Encoding             as T
+import qualified Data.Text.IO                   as T
+import qualified Data.Text.Encoding.UTF7.IMAP   as T
 
 #ifdef TestSgfSplit
 import Control.Monad
@@ -14,14 +15,16 @@ import qualified Sgf.Text.IMAPUtf7 as S
 
 main :: IO ()
 main                = do
-    c <- C.getContents
-    let xs = map T.decodeUtf7  . C.lines $ c
+    l <- C.getContents
+    let cs = C.lines l
+        xs = map T.decodeUtf7 cs
 #ifdef TestSgfSplit
-    let ys = map S.decodeUtf7B . C.lines $ c
+    let ys = map S.decodeUtf7B cs
         zs = filter (uncurry (/=)) $ zip xs ys
     forM_ zs $ \(x, y) -> hPutStrLn stderr $ "Results do not match:\n"
       ++ "text-utf7: '" ++ T.unpack x ++ "'\n"
       ++ "mine: '"    ++ T.unpack y ++ "'\n"
 #endif
-    T.putStr (T.unlines xs)
+    let inLine c x = T.decodeUtf8 c `T.append` T.pack "|" `T.append` x
+    T.putStr . T.unlines $ zipWith inLine cs xs
 
