a51e9ae7628885b8042c2d35fa3bdaae97c43f6f
sgf <sgf.dma@gmail.com>
2017-01-13 14:35:35 +0300

    new: build. Use cabal flag to decode results using my split and compare with just `decodeUtf7`.

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,24 @@
+{-# LANGUAGE CPP #-}
 
-import Control.Monad
 import qualified Data.ByteString.Char8 as C
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Data.Text.Encoding.UTF7.IMAP as T
 
+#ifdef TestSgfSplit
 import qualified Sgf.Text.IMAPUtf7 as S
+#endif
 
 
 main :: IO ()
 main                = do
     c <- C.getContents
     let c' = T.unlines . map T.decodeUtf7 . C.lines $ c
+#ifdef TestSgfSplit
     let d' = T.unlines . map S.decodeUtf7B . C.lines $ c
     if (c' /= d')
       then putStrLn "Warning, does not match"
       else putStrLn "Matches.."
+#endif
     T.putStr c'
 
